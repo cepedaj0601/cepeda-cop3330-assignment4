f321e45d3a3f071f9690de09398af06546b059c6
Justin Laptop <cepedaj0601@gmail.com>
2021-07-01 02:06:50 -0400

    Post Lecture Commit

---
+++ src/main/java/ucf/assignments/ToDoListController.java
@@ -0,0 +1,43 @@
+package ucf.assignments;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class ToDoListController {
+
+    @FXML
+    private TextField resultDisplay;
+
+    @FXML
+    public void allItemsButtonClicked(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void completedItemsButtonClicked(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void incompleteItemsButtonClicked(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void deleteCurrentListButtonClicked(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void loadButtonClicked(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void exportButtonClicked(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void editButtonClicked(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void addListButtonClicked(ActionEvent actionEvent) {
+    }
+}
---
+++ src/test/java/ucf/assignments/ToDoListControllerTest.java
@@ -0,0 +1,41 @@
+package ucf.assignments;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ToDoListControllerTest {
+
+    @Test
+    void allItemsButtonClicked() {
+
+    }
+
+    @Test
+    void completedItemsButtonClicked() {
+    }
+
+    @Test
+    void incompleteItemsButtonClicked() {
+    }
+
+    @Test
+    void deleteCurrentListButtonClicked() {
+    }
+
+    @Test
+    void loadButtonClicked() {
+    }
+
+    @Test
+    void exportButtonClicked() {
+    }
+
+    @Test
+    void editButtonClicked() {
+    }
+
+    @Test
+    void addListButtonClicked() {
+    }
+}
