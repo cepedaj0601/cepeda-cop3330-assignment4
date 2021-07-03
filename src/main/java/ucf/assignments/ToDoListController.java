94cdb05b7657d0f3b7e34811b3e77c801fe2382d
Justin Laptop <cepedaj0601@gmail.com>
2021-07-02 23:57:37 -0400

    first set of comments Commit

--- src/main/java/ucf/assignments/ToDoListController.java
+++ src/main/java/ucf/assignments/ToDoListController.java
@@ -11,36 +11,65 @@ public class ToDoListController {
 
     @FXML
     public void allItemsButtonClicked(ActionEvent actionEvent) {
+        //
     }
 
     @FXML
     public void completedItemsButtonClicked(ActionEvent actionEvent) {
+        //
     }
 
     @FXML
     public void incompleteItemsButtonClicked(ActionEvent actionEvent) {
+        //
     }
 
     @FXML
     public void deleteCurrentListButtonClicked(ActionEvent actionEvent) {
+        //
     }
 
     @FXML
     public void addListButtonClicked(ActionEvent actionEvent) {
+
     }
 
+    @FXML
     public void exportCurrentListButtonClicked(ActionEvent actionEvent) {
+        //take in all stored variables (list name, list items, marked as complete variable, etc)
+        //create a new variable that stores the file directory
+        //prompt for the file directory to be stored in a string
+        //create a new txt file under the new file directory
+        //copy over the list name
+        //in a loop the size of the number of items in the list, copy each item due date, name, and description
+        //save the new file with the copied elements
     }
 
+    @FXML
     public void exportAllListsButtonClicked(ActionEvent actionEvent) {
+        //take in list counter
+        //in a for loop the size of the list counter, do the following:
+        //take in all stored variables (list name, list items, marked as complete variable, etc) for the current list
+        //create a new variable that stores the file directory
+        //prompt for the file directory to be stored in a string(all lists will be stored here)
+        //create a new txt file for the current list under the new file directory
+        //copy over the list name
+        //in a loop the size of the number of items in the list, copy each item due date, name, and description
+        //save the new file with the copied elements
+        //repeat n times
     }
 
+    @FXML
     public void loadListButtonClicked(ActionEvent actionEvent) {
+        //
     }
 
+    @FXML
     public void loadMultipleListsButtonClicked(ActionEvent actionEvent) {
+        //
     }
 
     public void editNameButtonClicked(ActionEvent actionEvent) {
+        //
     }
 }
